bee355d1216f9534caf878b3dc84cc9a4f52708a
TrueWarg <irrmm9000@gmail.com>
2022-03-22 00:32:43 +0300

    Impl modify using block descriptors

--- src/Vorbild/Modifieble/Block.hs
+++ src/Vorbild/Modifieble/Block.hs
@@ -2,15 +2,16 @@
 
 module Vorbild.Modifieble.Block where
 
-import qualified Data.Text as T
+import           Data.List(sort, sortBy)
+import qualified Data.Text    as T
+import           Vorbild.Text (breakOnThree)
 
 data Descriptor =
   Descriptor
-    { start    :: Maybe T.Text
-    , end      :: Maybe T.Text
-    , body     :: T.Text
-    , actions  :: [Action]
-    , children :: [Descriptor]
+    { dStart    :: T.Text
+    , dEnd      :: T.Text
+    , dActions  :: [Action]
+    , dChildren :: [Descriptor]
     }
 
 data Action
@@ -19,13 +20,29 @@ data Action
   | SortLines
   | SortLinesDesc
 
+modify :: T.Text -> [Descriptor] -> T.Text
+modify text [] = text
+modify text (descriptor:descriptors) =
+  modifySingle (modify text (dChildren descriptor <> descriptors)) descriptor
+
+modifySingle :: T.Text -> Descriptor -> T.Text
+modifySingle text descriptor =
+  let start = dStart descriptor
+      end = dEnd descriptor
+      actions = dActions descriptor
+      result = breakOnThree start end text
+   in case result of
+        (Left err) -> text
+        Right (beforeStartIncl, body, afterEndIncl) ->
+          beforeStartIncl <> applyActionList body actions <> afterEndIncl
+
 applyAction :: T.Text -> Action -> T.Text
 applyAction block action =
   case action of
     (Append other)  -> block <> other
     (Prepend other) -> other <> block
-    SortLines       -> sort $ T.lines block
-    SortLinesDesc   -> sortBy (flip compare) $ T.lines block
+    SortLines       -> T.concat $ sort $ T.lines block
+    SortLinesDesc   -> T.concat $ sortBy (flip compare) $ T.lines block
 
 applyActionList :: T.Text -> [Action] -> T.Text
 applyActionList block [] = block
--- src/Vorbild/Text.hs
+++ src/Vorbild/Text.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Vorbild.Text
   ( splitOnAnyOf
+  , breakOnThree
   ) where
 
 import qualified Data.Text as T
@@ -7,3 +10,34 @@ import qualified Data.Text as T
 splitOnAnyOf :: [T.Text] -> T.Text -> [T.Text]
 splitOnAnyOf separators txt =
   foldl (\acc separator -> acc >>= T.splitOn separator) [txt] separators
+
+data BreakOnThreeError
+  = EmptyArg
+  | Fail
+
+breakOnThree ::
+     T.Text
+  -> T.Text
+  -> T.Text
+  -> Either BreakOnThreeError (T.Text, T.Text, T.Text)
+breakOnThree "" _ _ = Left EmptyArg
+breakOnThree _ "" _ = Left EmptyArg
+breakOnThree start end txt =
+  if (T.null beforeStartIncl || T.null afterStart)
+    then Left Fail
+    else Right (beforeStartIncl, body, afterEndIncl)
+    -- OK
+    -- "aaaaaaaaSTARTaaaaaaaaENDaaa"
+    -- "aaaaaaaaSTART", "aaaaaaaa", "ENDaaa"
+    -- BAD
+    -- "aaaaaaaaSTARTaaaaaaaaaaaaaa"
+    -- "aaaaaaaaSTART", "", "aaaaaaaaaaaaaa"
+    -- BAD
+    -- "aaaaaaaaENDaaaaaaaaaaaaaa"
+    -- "", "aaaaaaaa", "ENDaaaaaaaaaaaaaa"
+    -- BAD
+    -- "aaaaaaaaaaaaaaaaaaaaaaaaa"
+    -- "", "aaaaaaaaaaaaaaaaaaaaaaaaa", "", "aaaaaaaaaaaaaaaaaaaaaaaaa"
+  where
+    (beforeStartIncl, afterStart) = T.breakOnEnd start txt
+    (body, afterEndIncl) = T.breakOn end afterStart
